9f9e97b03c216a74d831b46dcd3683c990be27ac
judvelasco29 <201980205@psu.palawan.edu.ph>
2022-03-07 23:57:36 +0800

    judy committed web2 and web3

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,15 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-        
+        if(isset($_POST['username'], $_POST['dish'], $_POST['color'])){
+            $name = ($_POST['username']);
+            $dish = ($_POST['dish']);
+            $color = ($_POST['color']);
+            echo "<h2> Thanks for your selection {$name} <br>
+                You really enjoy {$dish} <br>
+             - especially with a nice {$color} wine</h2>";
+        }
+        else { echo "Hi please select and enter your food and drinks. Thank you:-)"; }
     ?>
 </body>
 </html>
--- web3/calc.php
+++ web3/calc.php
@@ -2,13 +2,30 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+
+        $v1=$_POST['val1'];
+        $v2=$_POST['val2'];
+        $operators=$_POST['calc'];
+
         # test if input is_numeric
-        if(1){
+        if(is_numeric($v1) && is_numeric($v2)){
+
             # test if calc is not null
-            if(1){
+            if(!empty($operators)){
+                
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($operators){
+                    case "add":
+                        $result =  $v1+$v2;
+                        break;
+                    case "sub":
+                        $result = $v1-$v2;
+                        break;
+                    case "mul":
+                        $result = $v1*$v2;
+                        break;
+                    case "div":
+                        $result = $v1/$v2;
                 }
                 echo "Calculation result: $result";
             }
